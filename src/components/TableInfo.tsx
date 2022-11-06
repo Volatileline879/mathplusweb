1385d04c4db85bb9d7d59f033852c5704b5b1e34
Volatileline879 <volatileline879@gmail.com>
2022-11-06 13:02:55 -0300

    first commit

---
+++ src/components/TableInfo.tsx
@@ -0,0 +1,13 @@
+import '../styles/global.css'
+
+export function TableInfo() {
+  return (
+    <>
+      <tr className="flex gap-1">
+        <th className="flex w-16 h-16 rounded-md items-center justify-center bg-gray-200">1</th>
+        <th className="flex w-80 h-16 rounded-md items-center justify-center bg-gray-200">User name</th>
+        <th className="flex w-48 h-16 rounded-md items-center justify-center bg-sky-300">xp 000000</th>
+      </tr>
+    </>
+  )
+}
